eef1af553c1a4cdf6956884ac5f3afc68874cf30
JALG27 <41845529+JALG27@users.noreply.github.com>
2023-03-23 18:57:36 -0400

    BETA

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,33 +5,68 @@ const mongoose = require('mongoose');
 const User = mongoose.model("User");
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const nodemailer = require("nodemailer");
 
 //router.get('/home',(req,res)=>{
 //    res.send("Hello World");
 //})
 
-//async function mailer(recieveremail, code){   //posible implementacion
-   // console.log("Mailer function called");
-//}
+async function mailer(recieveremail, code){   
+    //console.log("Mailer function called");
+    let transporter = nodemailer.createTransport({
+        host:"smtp.gmail.com",
+        port: 587,
+
+        secure: false,
+        requireTLS: true,
+        auth: {
+            user: process.env.NodeMailer_email,
+            pass: process.env.NodeMailer_pass,
+        },
+    });
+
+    let info = await transporter.sendMail({
+        from: "Twitter",
+        to: `${recieveremail}`,
+        subject: "Verificacion de Email",
+        text: `Tu codigo de verificacion es ${code}`,
+        html: `<b>Tu codigo de verificacion es ${code}</b>`,
+    })
+
+    console.log("Mensaje enviado: %s", info.messageId);
+    console.log("URL anterior: %s", nodemailer.getTestMessageUrl(info));
+}
 
 router.post('/verify',(req,res)=>{
-    console.log('sent by client', req.body);
+    //console.log('sent by client', req.body);
     const {email} = req.body;
 
     if(!email){
         return res.status(422).json({error: "Porfavor llena todos los campos"});
     }
-    User.findOne({ email: email }).then(
-        (savedUser) => {
-            if(savedUser){
-                return res.status(422).json({error: "Credenciales invalidas"});
-            }
+    else {
+    User.findOne({ email: email })
+    .then(async (savedUser) => {
+        //console.log(savedUser);
+        //return res.status(200).json({error: "Email Enviado"});
+        if(savedUser){
+            return res.status(422).json({error: "Credenciales invalidas"});
         }
-    )     
-    
-})
+        try{
+            let VerificationCode = Math.floor(100000 + Math.random() * 900000)
+            await mailer(email,VerificationCode);
 
-router.post('/changeusername', (req,res) => {
+            return res.status(200).json({message: "Email enviado", VerificationCode, email});
+
+        }
+        catch(err){}
+        })
+    }
+})
+            
+     
+
+/*router.post('/changeusername', (req,res) => {
     const { username, email } = req.body;
     User.find({username}).then(async (savedUser) => {
         if(savedUser.length > 0){
@@ -70,5 +105,5 @@ router.post('/signup', async (req, res) => {
         }
     }
 })
-
+*/
 module.exports = router;
